892c8f89f0ee5dddd3ab0903023deb26d16553d7
Gold <ferquirozvargas@gmail.com>
2025-08-28 16:15:22 -0500

    edits

---
+++ control/toggle.js
@@ -0,0 +1,9 @@
+function toggleContent() {
+    var x = document.getElementById("generated-list");
+
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
--- views/admin.js
+++ views/admin.js
@@ -2,10 +2,22 @@ const templateAdminPage = () => /*html*/`
     <div>
         <h1>Student info chart</h1>
         <div>
-            <button hx-get="/student-list" hx-target="#generated-list" hx-swap="innerHTML" class="student-list-button">Lista de estudiantes</button>
+            <button hx-get="/student-list" hx-target="#generated-list" hx-swap="innerHTML" class="student-list-button" onclick="toggleContent()">Lista de estudiantes</button>
             <div id="generated-list"></div>
         </div>
     </div>
+
+    <script>
+        function toggleContent() {
+            var x = document.getElementById("generated-list");
+
+            if(x.style.display === "none"){
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
+    </script>
 `
 
 export default templateAdminPage;
