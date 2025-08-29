8ed3277ae8fb5bf62bb9c52a070c8b7f3db226f8
Gold <ferquirozvargas@gmail.com>
2025-08-29 07:29:00 -0500

    Style and admin view update

--- views/admin.js
+++ views/admin.js
@@ -3,6 +3,7 @@ const templateAdminPage = () => /*html*/`
         <h1>Student info chart</h1>
         <div>
             <button hx-get="/student-list" hx-target="#generated-list" hx-swap="innerHTML" class="student-list-button" onclick="toggleContent()">Lista de estudiantes</button>
+            
             <div id="generated-list"></div>
         </div>
     </div>
@@ -10,11 +11,14 @@ const templateAdminPage = () => /*html*/`
     <script>
         function toggleContent() {
             var x = document.getElementById("generated-list");
+            console.log("0");
 
-            if(x.style.display === "none"){
-                x.style.display = "block";
-            } else {
+            if(x.style.display != "none"){
+                console.log("1");
                 x.style.display = "none";
+            } else {
+                x.style.display = "block";
+                console.log("2");
             }
         }
     </script>
