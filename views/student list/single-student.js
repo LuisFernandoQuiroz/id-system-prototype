01f4bedd63086ac5aae5fbe6b465ec83755d4472
Gold <ferquirozvargas@gmail.com>
2025-10-12 16:14:52 -0500

    Minor display edit

--- views/student list/single-student.js
+++ views/student list/single-student.js
@@ -1,15 +1,17 @@
 export default function templateSingleStudent(key, value) {
     return /*html*/`
         <tr id="${key}">
-            <td><input type="text" value="${key}"></td>
-            <td>${value.generacion}</td>
-            <td>${value.carrera}</td>
-            <td>${value.grupo}</td>
-            <td>${value.nombre}</td>
-            <td>${value.apellidoPaterno}</td>
-            <td>${value.apellidoMaterno}</td>
-            <td>${value.CURP}</td>
-            <td id="table-button-column"><button id="table-button">DELETE</button></td>
+            <form hx-post="/update-student-row-data">
+                <td><input type="text" value="${key}"></td>
+                <td>${value.generacion}</td>
+                <td>${value.carrera}</td>
+                <td>${value.grupo}</td>
+                <td>${value.nombre}</td>
+                <td>${value.apellidoPaterno}</td>
+                <td>${value.apellidoMaterno}</td>
+                <td>${value.CURP}</td>
+                <td id="table-button-column"><input type="submit" value="EDIT" id="table-button"></td>
+            </form>
         </tr>
     `
 }
